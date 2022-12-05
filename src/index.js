b4ebb5b35b0d77900aafc111296a36a079ea7453
Jay <iasonas.tr@gmail.com>
2022-12-05 21:43:11 +0200

    added unineStemmer

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import lancasterStemmer from './lancaster-stemmer';
 import greekStemmer from "./greek-stemmer";
 import porterStemmer from "./porter-stemmer";
 import lovinsStemmer from "./lovins-stemmer";
+import unineStemmer from "./unine-stemmer";
 
-export {porterStemmer, lancasterStemmer, greekStemmer, lovinsStemmer};
+export {porterStemmer, lancasterStemmer, greekStemmer, lovinsStemmer, unineStemmer};
